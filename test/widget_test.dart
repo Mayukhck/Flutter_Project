973b167c7c42378b487c1a2854780e348422a724
Mayukhck <mayukhck@gmail.com>
2024-04-02 17:58:13 +0530

    05

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,7 @@ import 'package:first_app/dice_roller.dart';
 
 void main() {
   testWidgets('GradientContainer displays correctly', (WidgetTester tester) async {
-    // Build the widget
+    
     await tester.pumpWidget(
       const MaterialApp(
         home: Scaffold(
@@ -17,7 +17,7 @@ void main() {
       ),
     );
 
-    // Verify that GradientContainer is rendered with correct colors
+    
     final containerFinder = find.byType(Container);
     expect(containerFinder, findsOneWidget);
     final containerWidget = tester.widget<Container>(containerFinder);
@@ -26,27 +26,27 @@ void main() {
     final gradient = decoration.gradient as LinearGradient;
     expect(gradient.colors, equals([const Color.fromARGB(255, 18, 1, 46), const Color.fromARGB(255, 114, 73, 185)]));
 
-    // Verify that DiceRoller widget is rendered inside GradientContainer
+    
     expect(find.byType(DiceRoller), findsOneWidget);
   });
 
   testWidgets('Test DiceRoller Widget', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+    
     await tester.pumpWidget(const MaterialApp(home: Scaffold(body: DiceRoller())));
 
-    // Find the DiceRoller widget
+    
     final diceRollerFinder = find.byType(DiceRoller);
     expect(diceRollerFinder, findsOneWidget);
 
-    // Find the Roll Dice button
+    
     final rollDiceButtonFinder = find.text('Roll Dice');
     expect(rollDiceButtonFinder, findsOneWidget);
 
-    // Tap the Roll Dice button
+    
     await tester.tap(rollDiceButtonFinder);
     await tester.pump();
 
-    // Verify that the image updates after rolling the dice
+    
     expect(find.byType(Image), findsOneWidget);
   });
 }
